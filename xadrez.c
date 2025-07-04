54065811b265d9a2372a036fb06e5ed2f5f59220
Leilton22 <leosants488@gmail.com>
2025-07-04 15:38:46 +0000

    desafio modificado

--- xadrez.c
+++ xadrez.c
@@ -4,52 +4,47 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-int main() {
-   int i = 1;
-   int contador = 0;
-   int movimento = 1;
+//bom dia, segue abaixo a minha atividade, gostaria muito receber um feedback explicando se estou fazendo da maneira correta e onde estou errando.
 
-   for (int i = 0; i < 5; i++)
-   {
-     printf("Torre Para Frente!\n");
-   }
-   
-
-
-      while ( i <= 8)
-    {
-    printf("Rainha Para Esquerda!\n");
-    i++;
+//movimento do bispo
+void MoverBispo(int casas){
+    if(casas > 0) {
+        printf("Bispo Direita Cima\n");
+        MoverBispo(casas - 1);
     }
-   
-
-
- do {
-      printf("Bispo Cima Direita!\n", contador);  
-        contador++;
-
-    } while (contador < 5);
-
-      while (movimento--)
-   {
-    for (int i = 0; i < 2; i++){
-        printf("Cavalo Cima\n");
+}
+//movimento do Torre
+void MoverTorre(int casas){
+    if(casas > 0) {
+        printf("Torre direita\n");
+        MoverTorre(casas - 1);
     }
-    printf("Cavalo Direita \n");
-   }
-   
-  printf("Qual Movimento voce deseja fazer agora? \n");
-
-
-
+}
+//movimento do Rainha
+void MoverRainha(int casas){
+    if(casas > 0) {
+        printf("Rainha Esquerda\n");
+        MoverRainha(casas - 1);
+    }
+}
+// movimento do cavalo
+void MoverCavalo(int casas){
+    if(casas > 0) {
+        printf("Cavalo Cima Direita\n");
+        MoverCavalo(casas - 1);
+    }
+}
+//chamada dos operadores
+int main(){
+    MoverBispo(5);
+    MoverTorre(5);
+    MoverRainha(8);
+    MoverCavalo(1);
 
     return 0;
-
-    printf("Novo commit\n");
-  
 }
 
-//bispo 5 casas cima, direita, com estrutura while
-//rainha 8 casas esquerda, com estrutura do while
-//torre 5 casa para frente,  com estrutura for
-//cavalo 2 casas pra cima e 1 casa pra direita
+//Bispo 5 casas direita,cima
+//Torre 5 casa pra direita
+//Rainha 8 casas pra esquerda
+//cavalo 1 casa para cima direita
